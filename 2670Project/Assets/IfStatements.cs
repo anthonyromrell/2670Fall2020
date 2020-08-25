e14b6cc4364e5e0b90147bdad93ac69fdf32489c
Anthony Romrell <anthonyromrell@mac.com>
2020-08-24 22:28:19 -0600

    Ifs

--- 2670Project/Assets/IfStatements.cs
+++ 2670Project/Assets/IfStatements.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class IfStatements : MonoBehaviour
 {
+
+    public bool canRun;
+    public string password;
+    public int number;
+    
     // Start is called before the first frame update
     private void Start()
     {
-        Debug.Log("Hello World.");
+        if (canRun)
+        {
+            Debug.Log("Running");
+        }    
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        if (!canRun)
+        {
+            Debug.Log("Running");
+        }
+
+        Debug.Log(password == "OU812" ? "Correct" : "Incorrect");
+
+        Debug.Log(number >= 7 ? "Greater Than 7." : "Less Than 7.");
     }
 }
