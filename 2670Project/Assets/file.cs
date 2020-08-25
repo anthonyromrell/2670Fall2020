e8e65da47d04aa7402834afc3cf31ad4db84e01b
Anthony Romrell <anthonyromrell@mac.com>
2020-08-25 10:30:15 -0600

    Locked

--- 2670Project/Assets/file.cs
+++ 2670Project/Assets/file.cs
@@ -7,7 +7,7 @@ public class file : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Locked");
     }
 
     // Update is called once per frame
