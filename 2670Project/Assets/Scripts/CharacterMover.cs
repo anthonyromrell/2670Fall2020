af264232fa5f1c9921087d5c9c3fffddc497945a
Anthony Romrell <anthonyromrell@mac.com>
2020-09-02 14:15:01 -0600

    Updated Character Mover.

--- 2670Project/Assets/Scripts/CharacterMover.cs
+++ 2670Project/Assets/Scripts/CharacterMover.cs
@@ -1,48 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 movement;
-    public float gravity = -9.81f;
-    public float moveSpeed = 3f;
-    public float fastMoveSpeed;
-    public float jumpForce = 1f;
-    public int jumpCountMax = 2;
-    public int jumpCount;
-    public float rotateSpeed = 3f;
-    private Vector3 rotateMovement;
-    private bool groundedPlayer;
+
+    public float moveSpeed = 5f, rotateSpeed = 30f, gravity = -9.81f, jumpForce = 10f;
     private float yVar;
-    void Start()
+
+    public int jumpCountMax = 2;
+    private int jumpCount;
+    
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
     }
-    
-    void Update()
-    {
-        
-        movement.Set(moveSpeed*Input.GetAxis("Vertical"),yVar,0);
-        rotateMovement.y = rotateSpeed * Input.GetAxis("Horizontal");
-        controller.transform.Rotate(rotateMovement*Time.deltaTime);
-        movement = controller.transform.TransformDirection(movement);
 
-        groundedPlayer = controller.isGrounded;
+    private void Update()
+    {
+        var vInput = Input.GetAxis("Vertical")*moveSpeed;
+        movement.Set(vInput,yVar,0);
         
-        if (groundedPlayer && movement.y < 0)
+        
+        var hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
+        transform.Rotate(0,hInput,0);
+
+        yVar += gravity*Time.deltaTime;
+
+        if (controller.isGrounded && movement.y < 0)
         {
-            yVar = 0f;
+            yVar = -1f;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            yVar = jumpForce;
+            jumpCount++;
         }
         
-        yVar += gravity * Time.deltaTime;
-        
-        if (Input.GetButton("Jump") && groundedPlayer)
-        {
-            yVar += Mathf.Sqrt(jumpForce * -3.0f * gravity);
-        }
-        controller.Move(movement*Time.deltaTime);
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement * Time.deltaTime);
     }
 }
