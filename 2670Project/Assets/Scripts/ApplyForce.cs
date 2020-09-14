6c2f07fb2a828091c302cac8ce1be4197a2a0303
Anthony Romrell <anthonyromrell@mac.com>
2020-09-14 14:00:47 -0600

    Instancing

---
+++ 2670Project/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rBody;
+    public float force = 30f;
+    
+    
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        var forceDirection = new Vector3(force, 0, 0);
+        //forceDirection needs to be based on Player rotation (hint SO)
+        rBody.AddRelativeForce(forceDirection);
+    }
+
+}
---
+++ 2670Project/Assets/Scripts/Instancer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class Instancer : MonoBehaviour
+{
+    public GameObject prefab;
+    
+    //Make a method to call the Instance Method
+    
+    public void Instance()
+    {
+        var location = transform.position;
+        var rotationDirection = new Vector3(0f,45f, 0f);
+        Instantiate(prefab, location, Quaternion.Euler(rotationDirection));
+    }
+}
