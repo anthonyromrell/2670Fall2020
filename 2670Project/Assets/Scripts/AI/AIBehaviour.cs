8e7bde9d982f5ef37ea02ba2a2996ec4ad2aed05
Anthony Romrell <anthonyromrell@mac.com>
2020-10-12 14:17:39 -0600

    Hunting

---
+++ 2670Project/Assets/Scripts/AI/AIBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehaviour : MonoBehaviour
+{
+    private WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private NavMeshAgent agent;
+    public Transform player;
+    private bool canHunt, canPatrol;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //StartCoroutine(Patrol());
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        canHunt = true;
+        agent.destination = player.position;
+        var distance = agent.remainingDistance;
+        while (distance <= 0.25f)
+        {
+            distance = agent.remainingDistance;
+            yield return wffu;
+        }
+        yield return new WaitForSeconds(2f);
+        
+        if (canHunt)
+        {
+            StartCoroutine(OnTriggerEnter(other));
+        }
+        else
+        {
+            //StartCoroutine(Patrol());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canHunt = false;
+    }
+}
