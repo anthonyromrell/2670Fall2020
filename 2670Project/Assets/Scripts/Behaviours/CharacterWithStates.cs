6e89a71193bef14c265b988c61dea5ba1fc7017a
Anthony Romrell <anthonyromrell@mac.com>
2020-10-21 14:21:30 -0600

    StateMachine

---
+++ 2670Project/Assets/Scripts/Behaviours/CharacterWithStates.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterWithStates : MonoBehaviour
+{
+        private CharacterController controller;
+        public CharacterStateMachineData characterStates;
+
+        private Vector3 movement;
+        public float moveSpeed = 3;
+        public float gravity = -9.81f;
+        
+        
+        private void Start()
+        {
+                controller = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+                var newInput = Input.GetAxis("Vertical")*moveSpeed;
+                
+                switch (characterStates.value)
+                {
+                        case CharacterStateMachineData.characterStates.StandardWalk:
+                                movement.Set(newInput,gravity,0);
+                                print("Walk");
+                                break;
+                        case CharacterStateMachineData.characterStates.NoGravityWalk:
+                                movement.Set(newInput,0,0);
+                                print("Walk");
+                                break;
+                        case CharacterStateMachineData.characterStates.WallCrawl:
+                                movement.Set(0,newInput,0);
+                                print("Crawl");
+                                break;
+                        case CharacterStateMachineData.characterStates.KnockBack:
+                                print("KnockBack");
+                                break;
+                }
+
+                var newMovement = movement * Time.deltaTime;
+                controller.Move(newMovement);
+        }
+}
