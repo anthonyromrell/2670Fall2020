e881f3170124690b2b7a9332756a0dcd520ddedd
Anthony Romrell <anthonyromrell@mac.com>
2020-11-16 13:29:48 -0700

    Substance and PickUp

---
+++ 2670Project/Assets/Materials/SubstanceTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Substance.Game;
+using UnityEngine;
+
+public class SubstanceTest : MonoBehaviour
+{
+    public Substance.Game.SubstanceGraph obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj.SetInputBool("window", false);
+        obj.SetInputFloat("metal_rust_level", 1f);
+        obj.SetInputFloat("peeling_amount", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2670Project/Assets/Scripts/Behaviours/ObjectPickUpBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ObjectPickUpBehaviour : MonoBehaviour
+{
+    private Rigidbody rBody;
+    private bool canPickUp;
+    
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        canPickUp = Input.GetKey(KeyCode.E);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (canPickUp)
+        {
+            transform.parent = other.transform;
+            rBody.Sleep();
+        }
+        else
+        {
+            transform.parent = null;
+            rBody.WakeUp();
+        }
+    }
+}
